8244ca94874cecdfd6c108f148c95809dd3acb08
Jack Zeal <hakfoo@gmail.com>
2026-05-10 22:21:10 -0700

    Cache thumbnails because rendering more than a few on each redraw is sluggish

--- include/pager.h
+++ include/pager.h
@@ -14,6 +14,15 @@ struct wlr_scene_tree;
 struct view;
 enum border_type;
 
+struct thumbnail_cache {
+	uint64_t creation_id;
+	uint32_t age;
+	unsigned char *thumbnail;
+	int width;
+	int height;
+	struct thumbnail_cache *next;
+};
+
 void pager_create(void);
 void pager_update(void);
 void process_pager_release(float sx, float sy);
@@ -21,6 +30,8 @@ void process_pager_drag(float sx, float sy);
 void process_pager_press(float sx, float sy);
 void process_pager_move(float sx, float sy, struct view *found_view);
 struct view *find_pager_window(float sx, float sy);
+unsigned char *get_thumbnail_cache(struct output *output, struct view *view,
+	struct wlr_fbox border_fbox);
 
 extern struct view *active_drag_view;
 
