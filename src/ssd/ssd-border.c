30bd8bbc79768679db826da3a7fa9507a818cd15
Jack Zeal <hakfoo@gmail.com>
2026-05-10 18:15:15 -0700

    Realign borders for vertical titlebar

--- src/ssd/ssd-border.c
+++ src/ssd/ssd-border.c
@@ -156,31 +156,60 @@ ssd_border_update(struct ssd *ssd)
 	int top_x = ssd->titlebar.height <= 0 || ssd->state.was_squared
 		? 0
 		: theme->border_width + corner_width;
-
+	
 	enum ssd_active_state active;
 	FOR_EACH_ACTIVE_STATE(active) {
 		struct ssd_border_subtree *subtree = &ssd->border.subtrees[active];
 		if (theme->window[active].border_type) {
-			renderBufferset(subtree->textured_borders, full_width,
-				side_height+(ssd->titlebar.height + 2*theme->border_width),
-				-ssd->titlebar.height-theme->border_width);
+			// For left side
+			if (1 /*left side*/) {
+				renderBuffersetXY(subtree->textured_borders, full_width+(ssd->titlebar.height + theme->border_width),
+					side_height + 2*theme->border_width,
+					-ssd->titlebar.height-theme->border_width,-theme->border_width);
+			} else {
+				renderBufferset(subtree->textured_borders, full_width,
+					side_height+(ssd->titlebar.height + 2*theme->border_width),
+					-ssd->titlebar.height-theme->border_width);
+			}
 		} else {
-			wlr_scene_rect_set_size(subtree->left,
-				theme->border_width, side_height);
-			wlr_scene_node_set_position(&subtree->left->node,
-				0, side_y);
-			wlr_scene_rect_set_size(subtree->right,
-				theme->border_width, side_height);
-			wlr_scene_node_set_position(&subtree->right->node,
-				theme->border_width + width, side_y);
-			wlr_scene_rect_set_size(subtree->bottom,
-				full_width, theme->border_width);
-			wlr_scene_node_set_position(&subtree->bottom->node,
-				0, height);
-			wlr_scene_rect_set_size(subtree->top,
-				top_width, theme->border_width);
-			wlr_scene_node_set_position(&subtree->top->node,
-				top_x, -(ssd->titlebar.height + theme->border_width));
+			if (1 /*left side*/) {
+				wlr_scene_rect_set_size(subtree->left,
+					theme->border_width, side_height+2*theme->border_width);
+				wlr_scene_node_set_position(&subtree->left->node,
+					-ssd->titlebar.height-theme->border_width, -theme->border_width);
+				
+				wlr_scene_rect_set_size(subtree->right,
+					theme->border_width, side_height+2*theme->border_width);
+				wlr_scene_node_set_position(&subtree->right->node,
+					theme->border_width + width,-theme->border_width);
+				
+				wlr_scene_rect_set_size(subtree->bottom,
+					full_width+(ssd->titlebar.height + theme->border_width), theme->border_width);
+				wlr_scene_node_set_position(&subtree->bottom->node,
+					-ssd->titlebar.height-theme->border_width, height);
+				
+				wlr_scene_rect_set_size(subtree->top,
+					full_width+(ssd->titlebar.height + theme->border_width), theme->border_width);
+				wlr_scene_node_set_position(&subtree->top->node,
+					-ssd->titlebar.height-theme->border_width, -theme->border_width);
+			} else {
+				wlr_scene_rect_set_size(subtree->left,
+					theme->border_width, side_height);
+				wlr_scene_node_set_position(&subtree->left->node,
+					0, side_y);
+				wlr_scene_rect_set_size(subtree->right,
+					theme->border_width, side_height);
+				wlr_scene_node_set_position(&subtree->right->node,
+					theme->border_width + width, side_y);
+				wlr_scene_rect_set_size(subtree->bottom,
+					full_width, theme->border_width);
+				wlr_scene_node_set_position(&subtree->bottom->node,
+					0, height);
+				wlr_scene_rect_set_size(subtree->top,
+					top_width, theme->border_width);
+				wlr_scene_node_set_position(&subtree->top->node,
+					top_x, -(ssd->titlebar.height + theme->border_width));
+			}
 		}
 	}
 }
