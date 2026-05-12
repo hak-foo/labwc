b3770751d689ef406840e092e9d569bea2e274cd
Jack Zeal <hakfoo@gmail.com>
2026-05-11 19:45:20 -0700

    Explicit cache flushes and leave the idle windows' thumbnails cached much longer

--- src/action.c
+++ src/action.c
@@ -1651,7 +1651,9 @@ actions_run(struct view *activator, struct wl_list *actions, struct cursor_conte
 		struct view *view = view_for_action(activator, action, &ctx);
 
 		run_action(view, action, &ctx);
+		pager_flush(view);
 	}
 	// TODO: Do this after events, and do something meaningful
+	
 	pager_update();
 }
