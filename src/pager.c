459409a0f3c35c688dbe567c36061ddf2d12ce05
Jack Zeal <hakfoo@gmail.com>
2026-05-23 22:38:42 -0700

    Remove filtering on dragging past edge of pager

--- src/pager.c
+++ src/pager.c
@@ -177,14 +177,7 @@ void process_pager_release(void)
 }
 
 void process_pager_drag(float sx, float sy)
-{
-	if (sx < rc.pager_x + rc.theme->pager_border_width ||
-		sy < rc.pager_y + rc.theme->pager_border_width ||
-		sx > rc.pager_x + rc.pager_width - rc.theme->pager_border_width ||
-		sy > rc.pager_y + rc.pager_height - rc.theme->pager_border_width) {
-		return;
-	}
-	
+{	
 	sx -= rc.theme->pager_border_width;
 	sy -= rc.theme->pager_border_width;
 	if (active_drag_view) {
