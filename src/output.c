5ca1eea90357bee1ef03bb8ba235e8ac4f1c2d0e
Jack Zeal <hakfoo@gmail.com>
2026-06-01 19:54:04 -0700

    Remove dead icons when removing display

--- src/output.c
+++ src/output.c
@@ -300,6 +300,11 @@ handle_output_destroy(struct wl_listener *listener, void *data)
 		wlr_scene_node_destroy(&output->pager_osd->node);
 		output->pager_osd = NULL;
 	}
+	
+	if (output->icons_osd) {
+		wlr_scene_node_destroy(&output->icons_osd->node);
+		output->icons_osd = NULL;
+	}
 
 	struct view *view;
 	wl_list_for_each(view, &server.views, link) {
